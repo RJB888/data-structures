58283f0d8e7ab8f24ef9b4b5ab93e322ed375065
Rob Bronson <robert.j.bronson@gmail.com>
2017-12-10 15:42:45 -0800

    finished testing, set up timeit

--- src/bubble_sort.py
+++ src/bubble_sort.py
@@ -15,3 +15,30 @@ def bubble_sort(list):
                 list[i - 1] = temp
                 swap = True
     return list
+
+
+if __name__ == "__main__":  # pragma: no cover
+    import timeit as time
+    import random
+    reps = 100000
+    rev = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
+    fwd = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    rand_sm = random.sample(range(100), 20)
+    rand_lg = random.sample(range(2000), 500)
+    t1 = time.timeit("bubble_sort(rev)",
+                     number=reps,
+                     setup="from __main__ import bubble_sort, rev")
+    t2 = time.timeit("bubble_sort(fwd)",
+                     number=reps,
+                     setup="from __main__ import bubble_sort, fwd")
+    t3 = time.timeit("bubble_sort(rand_sm)",
+                     number=reps,
+                     setup="from __main__ import bubble_sort, rand_sm")
+    t4 = time.timeit("bubble_sort(rand_lg)",
+                     number=reps,
+                     setup="from __main__ import bubble_sort, rand_lg")
+
+    print("Reversed list: ", t1)
+    print("Ordered list: ", t2)
+    print("Randomized small list: ", t3)
+    print("Randomized large list: ", t4)
