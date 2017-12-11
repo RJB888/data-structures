246a9dd464e5914eca0ca6434f335b5fce6e989c
Rob Bronson <robert.j.bronson@gmail.com>
2017-12-10 17:59:16 -0800

    adding tox for testing

--- src/insertion.py
+++ src/insertion.py
@@ -3,6 +3,8 @@
 
 def insertion_sort(potato):
     """Sort by insertion method."""
+    if not potato:
+        raise IndexError
     for i in range(1, len(potato)):
         swap_index = i
         while swap_index > 0 and potato[swap_index] < potato[swap_index - 1]:
@@ -20,18 +22,18 @@ if __name__ == "__main__":  # pragma: no cover
     fwd = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
     rand_sm = random.sample(range(100), 20)
     rand_lg = random.sample(range(2000), 500)
-    t1 = time.timeit("bubble_sort(rev)",
+    t1 = time.timeit("insertion_sort(rev)",
                      number=reps,
-                     setup="from __main__ import bubble_sort, rev")
-    t2 = time.timeit("bubble_sort(fwd)",
+                     setup="from __main__ import insertion_sort, rev")
+    t2 = time.timeit("insertion_sort(fwd)",
                      number=reps,
-                     setup="from __main__ import bubble_sort, fwd")
-    t3 = time.timeit("bubble_sort(rand_sm)",
+                     setup="from __main__ import insertion_sort, fwd")
+    t3 = time.timeit("insertion_sort(rand_sm)",
                      number=reps,
-                     setup="from __main__ import bubble_sort, rand_sm")
-    t4 = time.timeit("bubble_sort(rand_lg)",
+                     setup="from __main__ import insertion_sort, rand_sm")
+    t4 = time.timeit("insertion_sort(rand_lg)",
                      number=reps,
-                     setup="from __main__ import bubble_sort, rand_lg")
+                     setup="from __main__ import insertion_sort, rand_lg")
 
     print("Reversed list: ", t1)
     print("Ordered list: ", t2)
