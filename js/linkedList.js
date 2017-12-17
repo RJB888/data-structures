7b03df514eaf953a7a922069d5a902a6c75763f0
Rob Bronson <robert.j.bronson@gmail.com>
2017-12-17 11:59:15 -0800

    installing js packages for testing and module creation.

---
+++ js/linkedList.js
@@ -0,0 +1,92 @@
+'use strict'
+
+class Node{
+
+  constructor(val){
+    this.value = val
+    this.next = null
+  }
+}
+
+class LinkedList{
+
+  constructor(){
+    this.head = null
+    this.size = 0
+  }
+
+  push(val){
+    let newNode = new Node(val)
+    this.head = newNode
+    this.size ++
+
+  }
+
+  pop(){
+    if(this.head === null){
+      return 'List is empty'
+    }
+    else{
+      let temp = this.head.value
+      this.head = this.head.next
+      this.size --
+      return temp
+    }
+
+  }
+
+  size(){
+    return this.size
+  }
+
+  search(val){
+    let tempNode = this.head
+    if (this.head === null){
+      return 'List is empty.'
+    }
+    try{
+      while(tempNode !== null){
+        if (tempNode.val === val){
+          return tempNode
+        }
+        else{
+          tempNode = tempNode.next
+        }
+      }
+    }
+    catch(e){
+      return 'Node not found.'
+    }
+  }
+
+  remove(val){
+    let curr = this.head
+    while (curr !== null ){
+      if (curr.next === val){
+        curr.next = curr.next.next
+        this.size --
+        return
+      }
+      curr = curr.next
+    }
+  }
+
+  display(){
+    let cur = this.head
+    let output = []
+    while (cur !== null ){
+      output.append(cur.val)
+      cur = cur.next
+    }
+    return output.join(', ')
+  }
+
+}
+
+//
+// push(val) will insert the value ‘val’ at the head of the list
+// pop() will pop the first value off the head of the list and return it. Raises an exception with an appropriate message if there are no values to return.
+// size() will return the length of the list
+// search(val) will return the node containing ‘val’ in the list, if present, else None
+// remove(node) will remove the given node from the list, wherever it might be (node must be an item in the list). If the node is not in the list, it should raise an exception with an appropriate message.
+// display() will return a unicode string representing the list as if it were a Python tuple literal: “(12, ‘sam’, 37, ‘tango’)”
